0dd7ba5db92b842464300b8da5ee26f73f518d74
Dardan Bujupaj <dardan@ax.tech>
2022-11-19 17:30:24 +0100

    await validate function

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { path } = req.query;
 
-  res.revalidate(path as string);
+  await res.revalidate(path as string);
 
   console.log(`Revalidated ${path}`);
 
